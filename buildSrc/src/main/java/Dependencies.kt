8c087f1f4761a28a3f456cc1879bd336882a182e
Nicolas Barcacci <nicolas.barcacci@protonmail.com>
2019-03-06 21:39:24 +0100

    Reorganise Dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,9 +1,12 @@
 private object DependenciesVersions {
+    const val androidXCoreKtx = "1.0.1"
+
     const val androidXAppCompat = "1.0.2"
     const val constraintLayout = "1.1.3"
+
     const val dagger = "2.21"
     const val navigation = "1.0.0-rc02"
-    const val androidXCoreKtx = "1.0.1"
+
     const val timber = "4.7.1"
 
     const val junit = "4.12"
@@ -14,12 +17,18 @@ object Dependencies {
     // https://github.com/JetBrains/kotlin/blob/master/ChangeLog.md
     const val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib:${PluginsVersions.kotlin}"
 
+    // Guide : https://developer.android.com/kotlin/ktx#kotlin
+    // Artifact : https://mvnrepository.com/artifact/androidx.core/core-ktx
+    const val androidXCoreKTX = "androidx.core:core-ktx:${DependenciesVersions.androidXCoreKtx}"
+
+
     // Artifact : https://mvnrepository.com/artifact/androidx.appcompat/appcompat
     const val androidXAppCompat = "androidx.appcompat:appcompat:${DependenciesVersions.androidXAppCompat}"
 
     // Guide : https://developer.android.com/training/constraint-layout/
     // Artifact : https://mvnrepository.com/artifact/androidx.constraintlayout/constraintlayout
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:${DependenciesVersions.constraintLayout}"
+
 
     // https://github.com/google/dagger
     const val dagger = "com.google.dagger:dagger:${DependenciesVersions.dagger}"
@@ -32,12 +41,10 @@ object Dependencies {
     // Artifact : https://mvnrepository.com/artifact/android.arch.navigation/navigation-fragment
     const val navigation = "android.arch.navigation:navigation-fragment:${DependenciesVersions.navigation}"
 
-    // Guide : https://developer.android.com/kotlin/ktx#kotlin
-    // Artifact : https://mvnrepository.com/artifact/androidx.core/core-ktx
-    const val androidXCoreKTX = "androidx.core:core-ktx:${DependenciesVersions.androidXCoreKtx}"
-
+    
     // https://github.com/JakeWharton/timber
     const val timber = "com.jakewharton.timber:timber:${DependenciesVersions.timber}"
+
 
     // https://github.com/junit-team/junit4
     const val junit = "junit:junit:${DependenciesVersions.junit}"
