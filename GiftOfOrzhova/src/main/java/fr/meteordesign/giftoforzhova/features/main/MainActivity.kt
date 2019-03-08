7ff0db87f3b8252538fb28d6975bfb0d9208a87b
Nicolas Barcacci <nicolas.barcacci@schibsted.com>
2019-03-08 17:12:17 +0100

    Create MainActivity Navigation Drawer

--- GiftOfOrzhova/src/main/java/fr/meteordesign/giftoforzhova/features/main/MainActivity.kt
+++ GiftOfOrzhova/src/main/java/fr/meteordesign/giftoforzhova/features/main/MainActivity.kt
@@ -1,10 +1,14 @@
 package fr.meteordesign.giftoforzhova.features.main
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.annotation.NavigationRes
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.navigation.NavigationView
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
@@ -16,7 +20,10 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.toolbar_main.*
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity(), HasSupportFragmentInjector, Navigator.Listener {
+class MainActivity : AppCompatActivity(),
+    HasSupportFragmentInjector,
+    NavigationView.OnNavigationItemSelectedListener,
+    Navigator.Listener {
 
     @Inject
     lateinit var fragmentInjector: DispatchingAndroidInjector<Fragment>
@@ -36,6 +43,7 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector, Navigator.
         setContentView(R.layout.activity_main)
 
         initToolbar()
+        initDrawerMenu()
 
         navigator.navigateToCardList(this)
     }
@@ -52,6 +60,30 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector, Navigator.
         }
         setSupportActionBar(toolbar_main)
     }
+
+    private fun initDrawerMenu() {
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawer_main_drawerLayout,
+            toolbar_main,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawer_main_drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_main_navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_main_drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawer_main_drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean = false
 
     override fun navigateTo(@NavigationRes graphRes: Int) {
         val graphFragment = NavHostFragment.create(graphRes)
