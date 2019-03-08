a1fdce072ea4770364e391d7cf48b6bb65c068f6
Nicolas Barcacci <nicolas.barcacci@schibsted.com>
2019-03-08 13:13:08 +0100

    Get app theme from repository

--- GiftOfOrzhova/src/main/java/fr/meteordesign/giftoforzhova/usecase/AppThemeUseCase.kt
+++ GiftOfOrzhova/src/main/java/fr/meteordesign/giftoforzhova/usecase/AppThemeUseCase.kt
@@ -4,9 +4,10 @@ import fr.meteordesign.repository.repositories.appsettings.AppSettingsRepository
 import fr.meteordesign.ui.UiAppTheme
 import javax.inject.Inject
 
+private val DEFAULT_THEME = UiAppTheme.ORZHOV
+
 class AppThemeUseCase @Inject constructor(
     private val appSettingsRepository: AppSettingsRepository
 ) {
-
-    fun getAppTheme(): UiAppTheme = UiAppTheme.ORZHOV
+    fun getAppTheme(): UiAppTheme = appSettingsRepository.getAppTheme()?.let { toUiAppTheme(it) } ?: DEFAULT_THEME
 }
