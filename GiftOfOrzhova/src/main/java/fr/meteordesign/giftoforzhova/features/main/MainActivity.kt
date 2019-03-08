872229219af9fba1878f05471b0286407726e19a
Nicolas Barcacci <nicolas.barcacci@protonmail.com>
2019-03-08 07:07:51 +0100

    Set toolbar depending theme

--- GiftOfOrzhova/src/main/java/fr/meteordesign/giftoforzhova/features/main/MainActivity.kt
+++ GiftOfOrzhova/src/main/java/fr/meteordesign/giftoforzhova/features/main/MainActivity.kt
@@ -6,6 +6,9 @@ import androidx.navigation.fragment.NavHostFragment
 import dagger.android.support.DaggerAppCompatActivity
 import fr.giftoforzhova.common.navigation.Navigator
 import fr.meteordesign.giftoforzhova.R
+import fr.meteordesign.ui.UiAppTheme
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar_main.*
 import javax.inject.Inject
 
 class MainActivity : DaggerAppCompatActivity(), Navigator.Listener {
@@ -14,13 +17,26 @@ class MainActivity : DaggerAppCompatActivity(), Navigator.Listener {
     lateinit var navigator: Navigator
 
     private val viewModel = MainViewModel()
+    private lateinit var appTheme: UiAppTheme
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(viewModel.appTheme.themeResId)
+        appTheme = viewModel.appTheme
+        setTheme(appTheme.themeResId)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initToolbar()
+
         navigator.navigateToCardList(this)
+    }
+
+    private fun initToolbar() {
+        val isDark = resources.getBoolean(appTheme.darkOnPrimaryResId)
+        toolbar_main_stub.apply {
+            layoutResource = if (isDark) R.layout.appbarlayout_main_dark else R.layout.appbarlayout_main_light
+            inflate()
+        }
+        setSupportActionBar(toolbar_main)
     }
 
     override fun navigateTo(@NavigationRes graphRes: Int) {
